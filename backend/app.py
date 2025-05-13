97b5c11f7c22806e3fb86f7c2b4d2691b1275a37
aungkowinitlay <aungkowin.osit@gmail.com>
2025-05-13 21:40:28 +0630

    add backend ip

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "http://192.168.1.8"}})
 
 @app.route("/api/message", methods=["GET"])
 def get_message():
